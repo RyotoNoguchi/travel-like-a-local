00692d7a484127ecb78c58ee48bc469173178faf
RyotoNoguchi <ryoto_noguchi@wamazing.jp>
2025-01-26 18:11:30 +0700

    💄 add logo onto Header component

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react'
 import Image from 'next/image'
+import { pacifico } from '@/app/ui/fonts'
+import { LOGO_TITLE } from '@/constants'
 
 type Props = {
   logo: {
@@ -9,7 +11,10 @@ type Props = {
 }
 
 export const Header: FC<Props> = ({ logo }) => (
-  <header className='flex gap-2 bg-white drop-shadow-sm'>
-    <Image src={logo.url} alt={logo.title} width={40} height={40} />
+  <header className='flex justify-center gap-2 bg-white drop-shadow-md h-14'>
+    <h1 className='flex items-center gap-2'>
+      <Image src={logo.url} alt={logo.title} width={40} height={40} />
+      <span className={`${pacifico.className} font-bold text-xl xs:text-2xl`}>{LOGO_TITLE}</span>
+    </h1>
   </header>
 )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,17 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
       },
-    },
+      screens: {
+        'xs': '390px'
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: []
+} satisfies Config
