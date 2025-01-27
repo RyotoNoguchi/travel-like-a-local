9acb1486357b2b9b45975835f410b9a649defcd3
RyotoNoguchi <ryoto_noguchi@wamazing.jp>
2025-01-27 09:20:25 +0700

    🌐 Apply internationalization

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -9,14 +9,7 @@ import { FavoriteIcon } from '@/app/ui/icons/favorite-icon'
 import { GlobeIcon } from '@/app/ui/icons/globe-icon'
 import { IconButton } from '@/app/ui/icon-button'
 import Link from 'next/link'
-
-const ICONS = [
-  { icon: <AreaIcon />, label: 'Area', href: '/area' },
-  { icon: <CategoryIcon />, label: 'Category', href: '/category' },
-  { icon: <SearchIcon />, label: 'Search', href: '/search' },
-  { icon: <FavoriteIcon />, label: 'Favorite', href: '/favorite' },
-  { icon: <GlobeIcon />, label: 'English', href: '/english' }
-]
+import { useTranslations } from 'next-intl'
 
 type Props = {
   logo: {
@@ -25,18 +18,28 @@ type Props = {
   }
 }
 
-export const Header: FC<Props> = ({ logo }) => (
-  <header className='flex justify-center sm:px-1 md:px-2 lg:px-4 sm:justify-between gap-2 bg-white drop-shadow-md h-14'>
-    <h1 className='flex items-center'>
-      <Link href='/' className='flex items-center gap-2 hover-animation' aria-label={LOGO_TITLE}>
-        <Image src={logo.url} alt={logo.title} width={40} height={40} />
-        <span className={`${pacifico.className} font-bold text-xl xs:text-2xl `}>{LOGO_TITLE}</span>
-      </Link>
-    </h1>
-    <nav className='hidden sm:flex items-center gap-2 text-xl'>
-      {ICONS.map(({ icon, label, href }) => (
-        <IconButton key={label} icon={icon} label={label} href={href} />
-      ))}
-    </nav>
-  </header>
-)
+export const Header: FC<Props> = ({ logo }) => {
+  const t = useTranslations('HomePage')
+  const icons = [
+    { icon: <AreaIcon />, label: t('area'), href: '/area' },
+    { icon: <CategoryIcon />, label: t('category'), href: '/category' },
+    { icon: <SearchIcon />, label: t('search'), href: '/search' },
+    { icon: <FavoriteIcon />, label: t('favorite'), href: '/favorite' },
+    { icon: <GlobeIcon />, label: t('language'), href: '/language' }
+  ]
+  return (
+    <header className='flex justify-center sm:px-1 md:px-2 lg:px-4 sm:justify-between gap-2 bg-white drop-shadow-md h-14'>
+      <h1 className='flex items-center'>
+        <Link href='/' className='flex items-center gap-2 hover-animation' aria-label={LOGO_TITLE}>
+          <Image src={logo.url} alt={logo.title} width={40} height={40} />
+          <span className={`${pacifico.className} font-bold text-xl xs:text-2xl `}>{LOGO_TITLE}</span>
+        </Link>
+      </h1>
+      <nav className='hidden sm:flex items-center gap-2 text-xl'>
+        {icons.map(({ icon, label, href }) => (
+          <IconButton key={label} icon={icon} label={label} href={href} />
+        ))}
+      </nav>
+    </header>
+  )
+}
