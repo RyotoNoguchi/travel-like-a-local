70b81dc499d1da5cb2c1109cf4e3a953a97ce550
RyotoNoguchi <ryoto_noguchi@wamazing.jp>
2025-01-29 08:28:04 +0700

    🧑‍💻 set up eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,49 @@ import { FlatCompat } from '@eslint/eslintrc'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+export const __filename = fileURLToPath(import.meta.url) // file:///Users/wa-pc261/Develop/travel-like-a-local/eslint.config.mjs
+const __dirname = dirname(__filename) // /Users/wa-pc261/Develop/travel-like-a-local
 
+// This is for next/core-web-vitals
 const compat = new FlatCompat({
   baseDirectory: __dirname
 })
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')]
+const eslintConfig = [
+  ...compat.config(
+    {
+      extends: ['next/core-web-vitals', 'next/typescript', 'next', 'prettier']
+    },
+    {
+      languageOptions: {
+        parser: '@typescript-eslint/parser',
+        parserOptions: {
+          project: './tsconfig.json'
+        }
+      }
+    },
+    {
+      rules: {
+        eqeqeq: 'error',
+        'no-console': 'warn',
+        'no-unused-vars': 'warn',
+        'no-implicit-globals': 'error',
+        'no-var': 'error',
+        'prefer-const': 'error',
+        'no-multi-spaces': 'error',
+        'consistent-return': 'error',
+        'no-duplicate-imports': 'error',
+        'no-useless-catch': 'warn',
+        'dot-notation': 'error',
+        'no-lonely-if': 'error',
+        'no-const-assign': 'error',
+        'no-new-object': 'error',
+        'no-array-constructor': 'error',
+        'no-implicit-coercion': 'error'
+      }
+    }
+  )
+]
 
 export default eslintConfig
+
