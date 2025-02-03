4368ccd7e537edb8369b691c2d521ad38959e12f
RyotoNoguchi <ryoto_noguchi@wamazing.jp>
2025-02-03 23:37:34 +0700

    🚧 💄Add Carousel

--- src/generated/gql.ts
+++ src/generated/gql.ts
@@ -1,7 +1,24 @@
 /* eslint-disable */
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
+import * as types from './graphql'
 
-const documents: any[] = []
+/**
+ * Map of all GraphQL operations in the project.
+ *
+ * This map has several performance disadvantages:
+ * 1. It is not tree-shakeable, so it will include all operations in the project.
+ * 2. It is not minifiable, so the string of a GraphQL query will be multiple times inside the bundle.
+ * 3. It does not support dead code elimination, so it will add unused operations.
+ *
+ * Therefore it is highly recommended to use the babel or swc plugin for production.
+ * Learn more about it here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#reducing-bundle-size
+ */
+const documents = {
+  '\n  query GetAsset($id: String!) {\n    asset(id: $id) {\n      title\n      url\n    }\n  }\n': types.GetAssetDocument,
+  '\n  query ListFeaturedBlog {\n    pageBlogPostCollection(limit: 10, where: { featuredImage_exists: true }) {\n      items {\n        slug\n        featuredImage {\n          url\n          title\n        }\n      }\n    }\n  }\n':
+    types.ListFeaturedBlogDocument
+}
+
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  *
@@ -15,6 +32,19 @@ const documents: any[] = []
  * Please regenerate the types.
  */
 export function graphql(source: string): unknown
+
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(
+  source: '\n  query GetAsset($id: String!) {\n    asset(id: $id) {\n      title\n      url\n    }\n  }\n'
+): (typeof documents)['\n  query GetAsset($id: String!) {\n    asset(id: $id) {\n      title\n      url\n    }\n  }\n']
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(
+  source: '\n  query ListFeaturedBlog {\n    pageBlogPostCollection(limit: 10, where: { featuredImage_exists: true }) {\n      items {\n        slug\n        featuredImage {\n          url\n          title\n        }\n      }\n    }\n  }\n'
+): (typeof documents)['\n  query ListFeaturedBlog {\n    pageBlogPostCollection(limit: 10, where: { featuredImage_exists: true }) {\n      items {\n        slug\n        featuredImage {\n          url\n          title\n        }\n      }\n    }\n  }\n']
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {}
--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,10 +1,24 @@
 import { gql } from '@apollo/client'
 
 export const GET_ASSET_QUERY = gql`
-  query ($id: String!) {
+  query GetAsset($id: String!) {
     asset(id: $id) {
       title
       url
+    }
+  }
+`
+
+export const LIST_FEATURED_BLOG_QUERY = gql`
+  query ListFeaturedBlog {
+    pageBlogPostCollection(limit: 10, where: { featuredImage_exists: true }) {
+      items {
+        slug
+        featuredImage {
+          url
+          title
+        }
+      }
     }
   }
 `
