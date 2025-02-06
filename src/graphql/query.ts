3a1729713f45e1b2ed36b8f9c0fcd5ab5e1a02ac
RyotoNoguchi <ryoto_noguchi@wamazing.jp>
2025-02-06 22:19:09 +0700

    Tweak ListFeaturedBlog query

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -11,7 +11,7 @@ export const GET_ASSET_QUERY = gql`
 
 export const LIST_FEATURED_BLOG_QUERY = gql`
   query ListFeaturedBlog {
-    pageBlogPostCollection(limit: 10, where: { featuredImage_exists: true }) {
+    pageBlogPostCollection(where: { contentfulMetadata: { tags: { id_contains_some: "featured" } } }, limit: 10) {
       items {
         slug
         featuredImage {
