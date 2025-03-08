28afcc3b3b07825cbd5f7f951664681fb4c92b37
RyotoNoguchi <ryoto_noguchi@wamazing.jp>
2025-03-08 11:19:48 +0900

    ♻️ FormatIndex helper function

--- src/app/ui/components/molecules/table-of-contents.tsx
+++ src/app/ui/components/molecules/table-of-contents.tsx
@@ -1,4 +1,5 @@
 import { BLOCKS, type Document, type TopLevelBlock } from '@contentful/rich-text-types'
+import { useTranslations } from 'next-intl'
 import type { FC } from 'react'
 
 type Heading = {
@@ -10,16 +11,17 @@ type Heading = {
 type Props = { content: Document }
 
 export const TableOfContents: FC<Props> = ({ content }) => {
+  const t = useTranslations('TableOfContents')
   const headings = extractHeadings(content)
   if (headings.length === 0) return null
 
   return (
     <div className="bg-slate-50 p-4 rounded-md flex flex-col gap-4">
-      <h3 className="text-2xl font-bold">Contents</h3>
+      <h3 className="text-2xl font-bold">{t('title')}</h3>
       <ul className="flex flex-col gap-2">
         {headings.map((heading, i) => (
           <li key={heading.id} className="flex gap-1 text-xl">
-            <span className=" text-slate-500">{`0${i + 1}`}.</span>
+            <span className=" text-slate-500">{formatIndex(i)}</span>
             <span className="text-blue-300">|</span>
             <a href={`#${heading.id}`} className="text-blue-500 hover:text-blue-600">
               {heading.text}
@@ -73,4 +75,8 @@ const extractHeadings = (content: Document): Heading[] => {
 const getTextFromNode = (node: TopLevelBlock): string => {
   const textNode = node.content?.[0]
   return textNode?.nodeType === 'text' ? textNode.value || '' : ''
+}
+
+const formatIndex = (index: number): string => {
+  return `${(index + 1).toString().padStart(2, '0')}.`
 }
