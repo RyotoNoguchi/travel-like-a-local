c479f081b7902f9d615ca4727094ba3e2400c96b
RyotoNoguchi <ryoto_noguchi@wamazing.jp>
2025-03-13 09:59:27 +0900

    ♻️ Change the role of ArticlePage component to a Router

--- src/app/[locale]/articles/[[...path]]/page.tsx
+++ src/app/[locale]/articles/[[...path]]/page.tsx
@@ -1,27 +1,14 @@
 import { createApolloClient } from '@/apolloClient'
-import { ReportView } from '@/app/ui/components/atoms/anaylytics/viewcount'
-import { DateComponent } from '@/app/ui/components/atoms/date'
-import { CalendarIcon } from '@/app/ui/components/atoms/icons/calendar-icon'
-import { EyeIcon } from '@/app/ui/components/atoms/icons/eye-icon'
-import { Breadcrumbs } from '@/app/ui/components/molecules/breadcrumbs'
-import { RichText } from '@/app/ui/components/molecules/rich-text'
-import { TableOfContents } from '@/app/ui/components/molecules/table-of-contents'
-import { BreadcrumbJsonLd } from '@/app/ui/components/seo/breadcrumbs-jsonld'
-import { PopularArticleListContainer } from '@/app/ui/popular-article-list/container'
-import { CONCEPT_SCHEME, LANGUAGE, LOCALE_CODE_MAP, LOGO_TITLE, REDIS_KEYS } from '@/constants'
-import type { ListArticleQuery, ListArticleQueryVariables, PageBlogPostContent } from '@/generated/graphql'
+import { ArticleDetailContainer } from '@/app/ui/article-detail/container'
+import { CONCEPT_SCHEME, LANGUAGE, LOCALE_CODE_MAP, LOGO_TITLE } from '@/constants'
+import type { ListArticleQuery, ListArticleQueryVariables } from '@/generated/graphql'
 import { LIST_ARTICLE_QUERY } from '@/graphql/query'
 import { getAllArticles } from '@/lib/contentful/get-articles'
 import { getConceptSchemes } from '@/lib/contentful/get-concept-schemes'
 import { getConcepts } from '@/lib/contentful/get-concepts'
-import { generateBreadcrumbs } from '@/utils/breadcrumb-helper'
 import { parseArticlePath } from '@/utils/path-helper'
 import { formatNameForUrl, generateHref } from '@/utils/url-helpers'
-import { Redis } from '@upstash/redis'
-import classNames from 'classnames'
 import type { Metadata, NextPage } from 'next'
-import { getTranslations } from 'next-intl/server'
-import { notFound } from 'next/navigation'
 
 type Props = {
   params: Promise<{
@@ -108,84 +95,9 @@ export const generateStaticParams = async () => {
 const ArticlePage: NextPage<Props> = async ({ params }) => {
   const { locale, path } = await params
   const { slug, category, region, area, prefecture } = await parseArticlePath(path)
-  const client = createApolloClient()
-  const popularArticleListT = await getTranslations({ locale, namespace: 'PopularArticleList' })
-  const articleT = await getTranslations({ locale, namespace: 'Article' })
-  const { data } = await client.query<ListArticleQuery, ListArticleQueryVariables>({
-    query: LIST_ARTICLE_QUERY,
-    variables: {
-      slug,
-      locale: LOCALE_CODE_MAP[locale]
-    }
-  })
-  const article = data.pageBlogPostCollection?.items.find((item) => item?.slug === slug)
-  if (article === undefined || article === null) notFound()
-  const breadcrumbs = generateBreadcrumbs({
-    path,
-    article: {
-      slug: article.slug,
-      title: article.title,
-      contentfulMetadata: article.contentfulMetadata
-    },
-    category,
-    region,
-    area,
-    prefecture
-  })
-  const redis = Redis.fromEnv()
-  const views = (await redis.get<number>([REDIS_KEYS.PAGEVIEWS, REDIS_KEYS.NAMESPACE, slug].join(':'))) ?? 0
-  return (
-    <div className={classNames('w-full flex justify-center', 'pt-2', 'semi-lg:gap-10')}>
-      <BreadcrumbJsonLd locale={locale} breadcrumbs={breadcrumbs} />
-      <ReportView slug={slug} />
-      <div
-        className={classNames(
-          'flex flex-col gap-1 px-3 max-w-screen-xxs',
-          'xs:max-w-screen-xs',
-          'semi-sm:max-w-screen-semi-sm',
-          'sm:max-w-screen-sm',
-          'xs:px-4',
-          'sm:px-6',
-          'lg:max-w-screen-md'
-        )}
-      >
-        <Breadcrumbs breadcrumbs={breadcrumbs} />
-        <div className="flex flex-col pb-4 mb-2 border-b border-slate-200 border-solid gap-4">
-          <div className="flex gap-2">
-            <div className="flex gap-0.5 items-center px-1">
-              <CalendarIcon width={16} height={16} />
-              <DateComponent date={article?.publishedDate as string} className="h-4 text-slate-500 text-sm" />
-            </div>
-            <div className="flex gap-0.5 items-center">
-              <EyeIcon width={16} height={16} />
-              <p className="text-sm text-slate-500 h-[18px] flex gap-0.5">
-                {views}
-                <span>{articleT('views')}</span>
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col gap-1">
-            <h2 className="text-3xl font-bold">{article?.title}</h2>
-            {/* eslint-disable-next-line react/jsx-no-leaked-render */}
-            {article?.contentfulMetadata.tags && (
-              <ul className="flex flex-wrap gap-2">
-                {article?.contentfulMetadata.tags?.map((tag) => (
-                  <li key={tag?.name} className="bg-slate-100 rounded-sm px-2 py-1 text-sm text-slate-500">
-                    {tag?.name}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-        </div>
-        <div className="flex flex-col gap-5">
-          {article?.content ? <TableOfContents content={article.content.json} /> : null}
-          {article?.content && article.content.__typename === 'PageBlogPostContent' ? <RichText content={article.content as PageBlogPostContent} /> : null}
-        </div>
-      </div>
-      <PopularArticleListContainer title={popularArticleListT('title')} viewCountText={articleT('views')} />
-    </div>
-  )
+  // TODO: return ArticleList component instead of null when slug is falsy
+  if (!slug) return null
+  return <ArticleDetailContainer locale={locale} slug={slug} category={category} region={region} area={area} prefecture={prefecture} path={path} />
 }
 
 export default ArticlePage
