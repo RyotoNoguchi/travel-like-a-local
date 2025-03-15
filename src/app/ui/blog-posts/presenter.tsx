b6c18f30c6b6efd296c355d41fe8cd8f8b3c5e8c
RyotoNoguchi <ryoto_noguchi@wamazing.jp>
2025-03-15 18:11:36 +0900

    ♻️ ArticleList ➔ BlogPosts

--- src/app/ui/blog-posts/presenter.tsx
+++ src/app/ui/blog-posts/presenter.tsx
@@ -15,7 +15,7 @@ type Props = {
   totalPages?: number
 }
 
-export const ArticleList: FC<Props> = ({ blogPosts, title, viewAllButtonText, viewAllHref = '/articles', total, currentPage = 1, totalPages = 1 }) => (
+export const BlogPosts: FC<Props> = ({ blogPosts, title, viewAllButtonText, viewAllHref = '/articles', total, currentPage = 1, totalPages = 1 }) => (
   <section className="flex flex-col w-full gap-4 items-center sm:max-w-[640px] xl:max-w-[800px]">
     <div className="flex justify-between items-center w-full max-w-[300px] sm:max-w-[640px] xl:max-w-[800px]">
       <h2 className="w-full flex-1 text-2xl font-bold text-left">{title}</h2>
