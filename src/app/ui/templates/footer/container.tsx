f0451fafba698001cd4556b981dae6c04e96a9c4
RyotoNoguchi <ryoto_noguchi@wamazing.jp>
2025-03-30 20:47:02 +0900

    🔥 Remove Icon from FooterContainer

--- src/app/ui/templates/footer/container.tsx
+++ src/app/ui/templates/footer/container.tsx
@@ -1,6 +1,5 @@
 import { COLORS } from '@/app/ui/colors'
 import { AreaIcon } from '@/app/ui/components/atoms/icons/area-icon'
-import { CategoryIcon } from '@/app/ui/components/atoms/icons/category-icon'
 import { SearchIcon } from '@/app/ui/components/atoms/icons/search-icon'
 import { listSocialLinks } from '@/utils/socialLinks'
 import { useTranslations } from 'next-intl'
@@ -18,8 +17,7 @@ export const FooterContainer: FC<Props> = ({ logo }) => {
   const t = useTranslations()
   const navLinks = [
     { icon: <SearchIcon width={24} height={24} color={COLORS.WHITE} />, label: t('NavMenu.search'), href: '/search' },
-    { icon: <AreaIcon width={24} height={24} color={COLORS.WHITE} />, label: t('NavMenu.area'), href: '/area' },
-    { icon: <CategoryIcon width={24} height={24} color={COLORS.WHITE} />, label: t('NavMenu.category'), href: '/category' }
+    { icon: <AreaIcon width={24} height={24} color={COLORS.WHITE} />, label: t('NavMenu.area'), href: '/area' }
   ]
 
   const socialLinks = listSocialLinks({ width: 32, height: 32 })
