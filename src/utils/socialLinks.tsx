37f2ecb31a9a10a6f9aaab2c1578300746ecffa9
RyotoNoguchi <ryoto_noguchi@wamazing.jp>
2025-04-23 09:55:57 +0900

    💄 Tweak Social Link

--- src/utils/socialLinks.tsx
+++ src/utils/socialLinks.tsx
@@ -1,8 +1,8 @@
 import { FacebookIcon } from '@/app/ui/components/atoms/icons/facebook-icon'
 import { InstagramIcon } from '@/app/ui/components/atoms/icons/instagram-icon'
 import { TikTokIcon } from '@/app/ui/components/atoms/icons/tik-tok-icon'
-import { TwitterIcon } from '@/app/ui/components/atoms/icons/twitter-icon'
-import { WhatsAppIcon } from '@/app/ui/components/atoms/icons/whats-app-icon'
+// import { TwitterIcon } from '@/app/ui/components/atoms/icons/twitter-icon'
+// import { WhatsAppIcon } from '@/app/ui/components/atoms/icons/whats-app-icon'
 import { YouTubeIcon } from '@/app/ui/components/atoms/icons/youtube-icon'
 
 type Props = {
@@ -11,10 +11,10 @@ type Props = {
 }
 
 export const listSocialLinks = ({ width, height }: Props) => [
-  { icon: <TwitterIcon width={width} height={height} />, href: 'https://twitter.com/your_twitter_handle' },
-  { icon: <WhatsAppIcon width={width} height={height} />, href: 'https://wa.me/your_phone_number' },
-  { icon: <FacebookIcon width={width} height={height} />, href: 'https://www.facebook.com/your_facebook_page' },
-  { icon: <TikTokIcon width={width} height={height} />, href: 'https://www.tiktok.com/@your_tiktok_handle' },
-  { icon: <YouTubeIcon width={width} height={height} />, href: 'https://www.youtube.com/channel/your_youtube_channel_id' },
-  { icon: <InstagramIcon width={width} height={height} />, href: 'https://www.instagram.com/your_instagram_handle' }
+  // { icon: <TwitterIcon width={width} height={height} />, href: 'https://twitter.com/your_twitter_handle' },
+  // { icon: <WhatsAppIcon width={width} height={height} />, href: 'https://wa.me/your_phone_number' },
+  { icon: <FacebookIcon width={width} height={height} />, href: 'https://www.facebook.com/ryoto314.ng' },
+  { icon: <TikTokIcon width={width} height={height} />, href: 'https://www.tiktok.com/@travellikealocal.jp' },
+  { icon: <YouTubeIcon width={width} height={height} />, href: 'https://www.youtube.com/@travellikealocaljp' },
+  { icon: <InstagramIcon width={width} height={height} />, href: 'https://www.instagram.com/travel_like_a_local.jp/' }
 ]
